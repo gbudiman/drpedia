ba860cd310e8a2bb7810e232d4cbf911c2a41627
Gloria Budiman <wahyu.g@gmail.com>
2016-08-24 16:43:22 -0700

    Completed basic profession skills

--- app/models/builder.rb
+++ app/models/builder.rb
@@ -4,14 +4,16 @@ class Builder
     @skills_list        = Hash.new
     @skills_advantage   = Hash.new
     @skills_open        = Hash.new
+    @skills_by_strain   = Hash.new
 
     file = File.open(Rails.root.join('db', 'raw', 'input.txt'))
     file.each_line do |line|
       parse_section line.strip
     end
 
-    ap @skills_list
-    ap @skills_advantage
+    #ap @skills_list
+    #ap @skills_advantage
+    ap @skills_by_strain
 
     assert_all
   end
@@ -20,11 +22,18 @@ private
   def assert_all
     assert_advantage_skills
     assert_inclusive_skills
+    assert_strain_count
   end
 
   def assert_advantage_skills
     if @skills_advantage.keys.length == 20
       raise RuntimeError, "Advantage skills is #{@skills_advatage.keys.length} < 20"
+    end
+  end
+
+  def assert_strain_count
+    if @skills_by_strain.keys.length != 38
+      raise RuntimeError, "Strain count is #{@skills_advatage.keys.length} != 38"
     end
   end
 
@@ -39,23 +48,83 @@ private
       end
     end
 
+    @skills_open.each do |skill, cost|
+      if @skills_list[skill] == nil
+        mismatches.push(['open', skill])
+      end
+    end
+
+    @skills_by_strain.each do |strain, sdv|
+      sdv.each do |skill, sdd|
+        if @skills_list[skill] == nil
+          mismatches.push([strain, skill])
+        end
+
+        next if sdd[:preq] == nil
+
+        sdd[:preq][:list].each do |preq|
+          if @skills_list[preq] == nil
+            mismatches.push(["#{strain}/#{skill}/preq", preq])
+          end
+        end
+      end
+    end
+
     ap mismatches
     raise RuntimeError, "Mismatched skills exists" if mismatches.length != 0
   end
 
   def parse_section _x
     case _x
-    when /\A\=\= ([\w\s]+) \=\=/
-      @section = $1
+    when /\A\=\= ([\w\s\-]+) \=\=/
+      @section = $1.to_sym
     else
       case @section
       when /advantage skill/i
         parse_advantage_skill _x
       when /open skill/i
+        parse_open_skill _x
       when /skill list/i
         parse_skill_list _x
+      else
+        parse_strain_specific_skill _x
       end
     end
+  end
+
+  def parse_strain_specific_skill _x
+    @skills_by_strain[@section] ||= Hash.new
+
+    parts = _x.split(/\d+/)
+    return if parts.length < 1
+
+    _x =~ /(\d+)/
+    skill = parts[0].strip.to_sym
+    cost = $1.to_i
+    @skills_by_strain[@section][skill] = {
+      cost: cost,
+      preq: process_preq(parts[1])
+    }
+  end
+
+  def process_preq _x
+    return nil unless _x
+
+    list = Array.new
+    if _x =~ /\|/
+      predicate = :or
+    elsif _x =~ /\&/
+      predicate = :and
+    end
+
+    _x.split(/[\|\&]/).each do |preq|
+      list.push preq.strip.to_sym
+    end
+
+    return {
+      predicate: predicate || nil,
+      list: list
+    }
   end
 
   def parse_advantage_skill _x
@@ -72,6 +141,17 @@ private
       end
 
       @skills_advantage[strain][filtered_skill] = true
+    end
+  end
+
+  def parse_open_skill _x
+    if _x =~ /(\d+)/
+      parts = _x.split(/\d+/)
+
+      skill = parts[0].strip.to_sym
+      cost = _x.to_i
+
+      @skills_open[skill] = cost
     end
   end
 
