d77e8a2e092c4e4a3ac1586249d0834ffd7069a4
Gloria Budiman <wahyu.g@gmail.com>
2016-11-10 17:53:40 -0800

    Dynamic min-maxing on skill popover.

--- app/assets/javascripts/tree_builder.js
+++ app/assets/javascripts/tree_builder.js
@@ -21,7 +21,7 @@ var generate_strains_select_box = function() {
   $('#setup-strain').append(s);
   $('#strain-selector').multiselect({
     buttonWidth: '100%',
-
+    onDropdownShow: disable_popover,
     onChange: function(option, checked) {
       selected_strain = option.text();
       recalculate();
@@ -56,6 +56,7 @@ var generate_professions_select_box = function() {
         return labels.join(', ');
       }
     },
+    onDropdownShow: disable_popover,
     onChange: function(option, checked) {
       var selected_options = $('#profession-selector option:selected');
       if (selected_options.length >= 3) {
@@ -65,13 +66,13 @@ var generate_professions_select_box = function() {
 
         non_selected_options.each(function() {
           var input = $('input[value="' + $(this).val() + '"]');
-          input.prop('disabled', true);
+          input.prop('faded', true);
           input.parent().addClass('text-muted');
         })
       } else {
         $('#profession-selector option').each(function() {
           var input = $('input[value="' + $(this).val() + '"]');
-          input.prop('disabled', false);
+          input.prop('faded', false);
           input.parent().removeClass('text-muted');
         });
       }
@@ -94,11 +95,18 @@ var generate_skill_cat = function() {
             .addClass('list-group');
 
   $.each(skills, function(index, skill_name) {
+    // var a = $('<a></a>')
+    //           .addClass('clickable-skill')
+    //           .attr('href', '#')
+    //           .append(skill_name);
+
     var t = $('<li></li>')
-              .addClass('list-group-item disabled')
-              .attr('skill_name', skill_name)
-              .append(skill_name)
+              .addClass('list-group-item faded clickable-skill')
+              .attr('skill-name', skill_name)
+              .append('<span class="skill-label">' + skill_name + '</span>')
+              //.append(a)
               .append('<span class="badge"></span>');
+
     s.append(t);
   })
 
@@ -108,7 +116,95 @@ var generate_skill_cat = function() {
     .css('margin-left', '-4px')
     .css('margin-right', '0px');
   recalculate();
+  attach_anchor();
 };
+
+function attach_anchor() {
+  var target_element;
+  $('li.clickable-skill').on('click', function() {
+    disable_popover();
+
+    target_element = $(this).find('span.skill-label');
+    var skill_name = target_element.text();
+    var min_cost = parseInt($(this).find('span.badge').text());
+
+    if (target_element.attr('popover-applied') != 'true') {
+      target_element
+        .attr('popover-applied', true)
+        .attr('data-trigger', 'click')
+        .attr('data-placement', 'right')
+        .attr('data-html', true)
+    }
+    target_element.attr('data-content', pull_skill_cat_data(skill_name, min_cost));
+    target_element.popover('show');
+  })
+}
+
+function pull_skill_cat_data(skill, min_cost) {
+  var data = skill_cat[skill];
+  var by_strain = new Object();
+  var by_open = new Array();
+  var by_profession = new Object();
+  var s = '';
+
+  $.each(data, function(class_type, class_data) {
+    switch (class_type) {
+      case "innate":
+        $.each(class_data, function(index, innate_strain) {
+          by_strain[innate_strain] = 3;
+        }); 
+        break;
+      case "open":
+        by_open.push('Open: ' + class_data);
+        break;
+      case "innate_preq":
+        break;
+      default:
+        by_profession[class_type] = data[class_type];
+    }
+  });
+
+  if (Object.keys(by_strain).length > 0) {
+    $.each(by_strain, function(strain_name, cost) {
+      var class_name = 'text-muted';
+      if (strain_name == selected_strain) {
+        class_name = 'text-success';
+      }
+
+      if (cost == min_cost) {
+        class_name = 'text-primary';
+      }
+
+      var f = '<span class="' + class_name + '">' + strain_name + ': ' + cost + '</span>';
+      s += f + '<br />'
+    })
+
+    s += '<hr class="thin-divider" />';
+  }
+
+  if (by_open.length > 0) {
+    s += by_open.join('<br />') + '<hr class="thin-divider" />';
+  }
+
+  if (Object.keys(by_profession).length > 0) {
+    $.each(by_profession, function(profession_name, pdata) {
+      var class_name = 'text-muted';
+      if (selected_professions != undefined && 
+          selected_professions.indexOf(profession_name) != -1) {
+        class_name = 'text-success';
+      }
+
+      if (pdata.cost == min_cost) {
+        class_name = 'text-primary';
+      }
+
+      var f = '<span class="' + class_name + '">' + profession_name + ': ' + pdata.cost + '</span>';
+      s += f + '<br />'
+    })
+  }
+
+  return s;
+}
 
 function recalculate() {
   var detect_has_innate = function(data) {
@@ -140,21 +236,19 @@ function recalculate() {
     var min_cost = 99;
     var is_available = false;
     
-    // is_available = is_available || detect_is_profession_skill(data);
-    // is_available = is_available || detect_has_innate(data);
-    // is_available = is_available || detect_is_open_skill(data);
     min_cost = Math.min(min_cost, detect_is_profession_skill(data));
     min_cost = Math.min(min_cost, detect_has_innate(data));
     min_cost = Math.min(min_cost, detect_is_open_skill(data));
 
+    var o = $('[skill-name="' + skill_name + '"]');
     if (min_cost != 99) {
-      $('[skill_name="' + skill_name + '"]')
-        .removeClass('disabled')
-        .find('.badge').text(min_cost);
+      o.removeClass('faded')
+      o.find('.badge').text(min_cost)
+      o.find('.clickable-skill').removeClass('link-faded');
     } else {
-      $('[skill_name="' + skill_name + '"]')
-        .addClass('disabled')
-        .find('.badge').text('');
+      o.addClass('faded')
+      o.find('.badge').text('')
+      o.find('.clickable-skill').addClass('link-faded');
     }
   };
 
@@ -163,12 +257,16 @@ function recalculate() {
   });
 
   update_availability();
+  disable_popover();
 }
 
 function update_search_result(value) {
+  disable_popover();
+
   var pattern = new RegExp(value);
-  $('[skill_name]').each(function() {
-    var matcher = $(this).attr('skill_name').toLowerCase();
+  $('#only-available-checkbox').prop('checked', false);
+  $('[skill-name]').each(function() {
+    var matcher = $(this).attr('skill-name').toLowerCase();
 
     if (matcher.match(pattern)) {
       $(this).show();
@@ -179,15 +277,20 @@ function update_search_result(value) {
 }
 
 function update_availability() {
-  console.log('called');
+  disable_popover();
   var only_available = $('#only-available-checkbox').prop('checked');
 
   if (only_available) {
     $('.list-group-item').show();
-    $('.list-group-item.disabled').hide();
+    $('.list-group-item.faded').hide();
   } else {
-    $('.list-group-item.disabled').show();
+    $('.list-group-item.faded').show();
+
   }
+}
+
+function disable_popover() {
+  $('.popover').hide();
 }
 
 $(function() {
